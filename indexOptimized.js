9f06f47c4af5e3628982435575d850990e08caa5
aseemar <ajseemar@gmail.com>
2019-07-16 14:04:56 -0700

    push

--- indexOptimized.js
+++ indexOptimized.js
@@ -82,52 +82,209 @@ class Game {
 
 // A Star
 
+class MinHeap {
+    constructor() {
+        this.array = [null];
+    }
+
+    empty() {
+        if (this.array.length === 1) {
+            debugger
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getParent(idx) {
+        return Math.floor(idx / 2);
+    }
+
+    getLeftChild(idx) {
+        return idx * 2;
+    }
+
+    getRightChild(idx) {
+        return idx * 2 + 1;
+    }
+
+    insert(node) {
+        // add the new node to the bottom level, far-left 
+        this.array.push(node);
+
+        // then, sift that value up the heap to restore heap property
+        this.siftUp(this.array.length - 1);
+    }
+
+    siftUp(idx) {
+        // if the node is already at the root, there's no further we can sift up
+        if (idx === 1) return;
+
+        let parentIdx = this.getParent(idx);
+
+        // if the node is bigger than it's parent, we are breaking heap property...
+        if (this.array[parentIdx].node.f > this.array[idx].node.f) {
+            // so swap the node with it's parent
+            // [this.array[parentIdx], this.array[idx]] = [this.array[idx], this.array[parentIdx]];
+            let temp = this.array[parentIdx];
+            this.array[parentIdx] = this.array[idx];
+            this.array[idx] = temp;
+
+            // and continue to sift it up recursively
+            this.siftUp(parentIdx);
+        }
+    }
+
+    deleteMin() {
+        // recall that we have an empty position at the very front of the array, 
+        // so an array length of 2 means there is only 1 item in the heap
+
+        // if there is only 1 element in the heap, simply remove it
+        // debugger
+        if (this.array.length === 2) return this.array.pop();
+
+        // if there are no elements in the heap, do nothing
+        if (this.array.length === 1) return null;
+
+        // otherwise remove the last element and make it the root at the front of the array
+        let min = this.array[1];
+        this.array[1] = this.array.pop();
+
+        // then, sift the new root down to restore heap property
+        this.siftDown(1);
+        return min;
+    }
+
+    siftDown(idx) {
+        let ary = this.array;
+        let leftIdx = this.getLeftChild(idx);
+        let rightIdx = this.getRightChild(idx);
+        let leftVal = ary[leftIdx];
+        let rightVal = ary[rightIdx];
+
+        // if the node is missing children, consider the missing children as the value -Infinity
+        // this allows the node to keep heap property, since any value is greater than -Infinity
+        // this will also give us children values to compare later, undefined should not be used for comparison**
+        if (!leftVal) {
+            leftVal = {
+                node: {
+                    f: Infinity
+                }
+            };
+        }
+        if (!rightVal) {
+            rightVal = {
+                node: {
+                    f: Infinity
+                }
+            };
+        }
+        // if (leftVal.node.f === undefined) leftVal.node.f = Infinity;
+        // if (rightVal.node.f === undefined) rightVal.node.f = Infinity;
+
+        // if the node is bigger than both children, we have restored heap property, so exit
+        if (ary[idx].node.f < leftVal.node.f && ary[idx].node.f < rightVal.node.f) return;
+
+        // otherwise the node is smaller than one of it's children,
+        // so swap this node with the bigger between the two children**
+        if (leftVal.node.f < rightVal.node.f) {
+            var swapIdx = leftIdx;
+        } else {
+            var swapIdx = rightIdx;
+        }
+        let temp = ary[idx];
+        ary[idx] = ary[swapIdx];
+        ary[swapIdx] = temp;
+        // [ary[idx], ary[swapIdx]] = [ary[swapIdx], ary[idx]];
+
+        // and continue to sift it down recursively
+        this.siftDown(swapIdx);
+    }
+
+    includes(cell, idx = 1) {
+        return this.array.includes(cell);
+        // if (this.array.length === 1) return false;
+        // if (this.array[idx] === cell) return true;
+        // let leftIdx = this.getLeftChild(idx);
+        // let rightIdx = this.getRightChild(idx);
+        // let leftVal = this.array[leftIdx];
+        // let rightVal = this.array[rightIdx];
+        // if (this.array[idx] === leftVal) return true;
+        // if (this.array[idx] === rightVal) return true;
+
+        // if (!leftVal || leftVal.node.f > cell.node.f) return false;
+        // if (!rightVal || rightVal.node.f > cell.node.f) return false;
+
+        // if (this.includes(cell, leftIdx)) return true;
+        // else if (this.includes(cell, rightIdx)) return true;
+        // else return false;
+    }
+
+    clean() {
+        this.array = [null];
+    }
+}
 
 class aStar {
     constructor(grid) {
+        this.nodes = grid.cells.map(cell => cell.node);
         this.grid = grid;
         this.cells = grid.cells;
         this.start = this.cells[0];
         this.end = this.cells[this.cells.length - 1];
-        console.log(this.start, this.end);
+        // console.log(this.start, this.end);
 
         this.openSet = [this.start];
         this.closedSet = [];
         this.path = [];
+
+        // for optimized algo
+
+        // this.start = this.nodes[0];
+        // this.end = this.nodes[this.nodes.length - 1];
+        this.openHeap = new MinHeap();
+        this.openHeap.insert(this.start);
         this.initialTime = Date.now();
     }
 
     update() {
-        if (this.openSet.length > 0) {
-            let winner = 0;
-            this.openSet.forEach((cell, idx) => {
-                // debugger
-                if (cell.node.f < this.openSet[winner].node.f) winner = idx;
-                // debugger
-            });
+        if (!this.openHeap.empty()) {
+            // let winner = 0;
+            // this.openSet.forEach((cell, idx) => {
+            //     // debugger
+            //     if (cell.node.f < this.openSet[winner].node.f) winner = idx;
+            //     // debugger
+            // });
 
-            const current = this.openSet[winner];
-            this.path = [];
+            const current = this.openHeap.deleteMin();
+            // this.path = [];
+            // let temp = current.node;
+            // this.path.push(temp);
+            // while (temp.parent) {
+            //     this.path.push(temp.parent);
+            //     temp = temp.parent;
+            // }
             if (current === this.end) {
                 console.log(`${(Date.now() - this.initialTime) / 1000.0} seconds`)
+                console.log('DONE');
+                this.path = [];
                 let temp = current.node;
                 this.path.push(temp);
                 while (temp.parent) {
                     this.path.push(temp.parent);
                     temp = temp.parent;
                 }
-                console.log('DONE');
                 this.finished = true;
-                this.openSet = [];
                 return;
             }
+            // if (!current) debugger;
             // remove current from open set
-            for (let i = this.openSet.length - 1; i >= 0; i--) {
-                if (this.openSet[i] === current) {
-                    this.openSet.splice(i, 1);
-                    break;
-                }
-            }
+            // for (let i = this.openSet.length - 1; i >= 0; i--) {
+            //     if (this.openSet[i] === current) {
+            //         this.openSet.splice(i, 1);
+            //         break;
+            //     }
+            // }
             // add current to closed set 
             // debugger
             const neighbors = current.neighbors.filter(obj => !Object.keys(current.walls)
@@ -139,12 +296,12 @@ class aStar {
                 if (!this.closedSet.includes(neighbor)) {
                     const tentativeG = current.node.g + 1;
                     let newPath = false;
-                    if (this.openSet.includes(neighbor) && tentativeG < neighbor.node.g) {
+                    if (this.openHeap.includes(neighbor) && tentativeG < neighbor.node.g) {
                         neighbor.node.g = tentativeG;
                         newPath = true;
                     } else {
                         neighbor.node.g = tentativeG;
-                        this.openSet.push(neighbor);
+                        this.openHeap.insert(neighbor);
                         newPath = true;
                     }
 
